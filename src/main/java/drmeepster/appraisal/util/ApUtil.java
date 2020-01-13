a715a59ad82577afcfa1f066f589683e550b7000
DrMeepster <19316085+DrMeepster@users.noreply.github.com>
2020-01-12 17:02:34 -0800

    the great commentating

--- src/main/java/drmeepster/appraisal/util/ApUtil.java
+++ src/main/java/drmeepster/appraisal/util/ApUtil.java
@@ -1,7 +1,5 @@
 package drmeepster.appraisal.util;
 
-import java.lang.reflect.InvocationTargetException;
-
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -19,22 +17,16 @@ public class ApUtil{
 	public static final Style NO_APPRAISAL_STYLE = new Style().setColor(Formatting.GRAY).setItalic(true);
 	public static final Text NO_APPRAISAL_TEXT = new TranslatableText("appraisal.none").setStyle(NO_APPRAISAL_STYLE);
 
-	public static <T> String getKey(String type, T t, Registry<T> registry){ // Registry
-		return Util.createTranslationKey(type, registry.getId(t)) + ".";
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <T, U> U callAs(T caller, Class<? super T> clazz, String name, Class<?>[] paramTypes, Object... args)
-		throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
-		InvocationTargetException{
-		
-		return (U) clazz.getMethod(name, paramTypes).invoke(caller, args);
-	}
-	
-	public static <T, U> U callAs(T caller, Class<? super T> clazz, String name)
-		throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
-		InvocationTargetException{
-		
-		return callAs(caller, clazz, name, new Class<?>[]{});
+	/**
+	 * Creates a translation key
+	 * 
+	 * @param <T> Type of object the key is for
+	 * @param type Prefix for the key
+	 * @param obj The object to make a key for
+	 * @param registry A <code>Registry</code> containing <code>obj</code>
+	 * @return A translation key for <code>obj</code>
+	 */
+	public static <T> String getKey(String type, T obj, Registry<T> registry){ // Registry
+		return Util.createTranslationKey(type, registry.getId(obj)) + ".";
 	}
 }
